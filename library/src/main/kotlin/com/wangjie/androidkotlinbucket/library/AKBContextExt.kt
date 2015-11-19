b7c2dfc2dd90c4c3f8ce213419052a7f8def1d61
WangJie <tiantian.china.2@gmail.com>
2015-11-19 13:50:40 +0800

    Context增加isApplicationBackground扩展函数

--- library/src/main/kotlin/com/wangjie/androidkotlinbucket/library/AKBContextExt.kt
+++ library/src/main/kotlin/com/wangjie/androidkotlinbucket/library/AKBContextExt.kt
@@ -1,5 +1,6 @@
 package com.wangjie.androidkotlinbucket.library
 
+import android.app.ActivityManager
 import android.content.Context
 
 /**
@@ -10,3 +11,18 @@ import android.content.Context
  * 是否有SDCard
  */
 fun Context.haveSDCard() = android.os.Environment.getExternalStorageState() == android.os.Environment.MEDIA_MOUNTED
+
+/**
+ * 判断当前应用程序处于前台还是后台
+ * 需要添加权限: <uses-permission android:name="android.permission.GET_TASKS" />
+ */
+fun Context.isApplicationBackground(): Boolean {
+    val tasks = (getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).getRunningTasks(1)
+    if (!tasks.isEmpty()) {
+        val topActivity = tasks[0].topActivity
+        if (topActivity.packageName != packageName) {
+            return true
+        }
+    }
+    return false
+}
