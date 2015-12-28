89fdefcf065bf013cde52d60d62c8b3c9cee3d35
WangJie <tiantian.china.2@gmail.com>
2015-12-28 16:34:19 +0800

    增加C、P接口

--- library/src/main/kotlin/com/wangjie/androidkotlinbucket/library/AKBMVPExt.kt
+++ library/src/main/kotlin/com/wangjie/androidkotlinbucket/library/AKBMVPExt.kt
@@ -15,15 +15,10 @@ import android.widget.Toast
  */
 
 /**
- * 用于懒加载Presenter
- */
-public fun <T : KPresenter<*>> _presenter(f: () -> T) = lazy(LazyThreadSafetyMode.NONE) { f() }
-
-/**
  * MVP的View层，UI相关，Activity需要实现该interface
  * 它会包含一个Presenter的引用，当有事件发生（比如按钮点击后），会调用Presenter层的方法
  */
-public interface KViewer {
+public interface KIViewer {
     //    val onClickListener: ((view: View) -> Unit)?
     val context: Context?;
 
@@ -49,11 +44,11 @@ public interface KViewer {
     }
 
     fun showLoading(message: String) {
-        Log.w(KViewer::class.java.simpleName, "loadingDialog should impl by subclass")
+        Log.w(KIViewer::class.java.simpleName, "loadingDialog should impl by subclass")
     }
 
     fun cancelLoading() {
-        Log.w(KViewer::class.java.simpleName, "cancelLoadingDialog should impl by subclass")
+        Log.w(KIViewer::class.java.simpleName, "cancelLoadingDialog should impl by subclass")
     }
 
     fun <T : View> findView(resId: Int): T;
@@ -64,10 +59,14 @@ public interface KViewer {
  * MVP的Presenter层，作为沟通View和Model的桥梁，它从Model层检索数据后，返回给View层，它也可以决定与View层的交互操作。
  * 它包含一个View层的引用和一个Model层的引用
  */
-public open class KPresenter<V : KViewer>(var viewer: V) {
+public interface KIPresenter {
+    public fun closeAll()
+}
 
-    open public fun closeAll() {
-        Log.w(KViewer::class.java.simpleName, "closeAll in KPresenter should impl by subclass")
+public open class KPresenter<V : KIViewer>(var viewer: V) : KIPresenter {
+
+    override public fun closeAll() {
+        Log.w(KIViewer::class.java.simpleName, "closeAll in KPresenter should impl by subclass")
     }
 
 }
@@ -75,24 +74,48 @@ public open class KPresenter<V : KViewer>(var viewer: V) {
 /**
  * Controller，用于派发View中的事件，它在根据不同的事件调用Presenter
  */
-public abstract class KController<KV : KViewer, KP : KPresenter<*>>(val viewer: KV, presenterCreate: () -> KP) {
+public interface KIController {
+    /**
+     * 注册事件
+     */
+    fun bindEvents()
+
+    fun <T : View> getView(resId: Int): T;
+    fun closeAll()
+
+//    /**
+//     * 界面对用户显示(onResume)
+//     */
+//    fun onUserVisible() {
+//    }
+//
+//    /**
+//     * 界面对用户隐藏(onPause)
+//     */
+//    fun onUserInvisible() {
+//    }
+//
+//    /**
+//     * 界面销毁时回调,需要在BaseActivity或者BaseFragment等组件中调用
+//     */
+//    fun onDestroy() {
+//    }
+
+}
+
+public abstract class KController<KV : KIViewer, KP : KPresenter<*>>(val viewer: KV, presenterCreate: () -> KP) : KIController {
     protected val presenter: KP by lazy { presenterCreate() }
 
     private val viewCache: SparseArray<View> = SparseArray();
 
-    /**
-     * 注册事件
-     */
-    abstract fun bindEvents()
-
-    public fun <T : View> getView(resId: Int): T {
+    public override fun <T : View> getView(resId: Int): T {
         val view: View? = viewCache.get(resId)
         return view as T? ?: viewer.findView<T>(resId).apply {
             viewCache.put(resId, this)
         }
     }
 
-    public fun closeAll() = presenter.closeAll()
+    public override fun closeAll() = presenter.closeAll()
 
 
 }
